ebfc65cab77a08b9c3a378764ffd5bcdd82bdcf4
Nick Pafundi <nick@cloversites.com>
2013-07-23 15:41:22 -0700

    'silence' clips will now silence all other sounds

--- Web/ajax/sound.php
+++ Web/ajax/sound.php
@@ -4,24 +4,24 @@ if (!empty($_REQUEST['sounds'])) {
 	
 	require_once dirname(__FILE__) . "/" . "../includes/db.php";
 	
-	$soundToPlay = $_REQUEST['sounds'];
-	$soundToPlay = explode(",", $soundToPlay);
+	$sound = $_REQUEST['sounds'];
 	
-	foreach ($soundToPlay as $sound) {
-		
-		system('/usr/bin/afplay /Users/DesignAndDevelopment/Effects/' . $sound);
-		
-		// log it
-		$stmt = $conn->prepare('INSERT INTO soundsPlayed (sound, dateTime) VALUES(:sound, NOW())');
-		$stmt->bindParam(':sound', $sound);
-		$stmt->execute();
-		
-		
-		echo "sound of clover: " . $sound;
-		
-		// has some issues - would be good to use though since it ducks down the current music
-		// shell_exec('/usr/local/bin/fx ' . $sound);
+	if ($sound == "silence.mp3" || $sound == "earl-silence.mp3") {
+		system('killall afplay');
 	}
+	
+	system('/usr/bin/afplay /Users/DesignAndDevelopment/Effects/' . $sound);
+	
+	// log it
+	$stmt = $conn->prepare('INSERT INTO soundsPlayed (sound, dateTime) VALUES(:sound, NOW())');
+	$stmt->bindParam(':sound', $sound);
+	$stmt->execute();
+	
+	
+	echo "sound of clover: " . $sound;
+	
+	// has some issues - would be good to use though since it ducks down the current music
+	// shell_exec('/usr/local/bin/fx ' . $sound);
 }
 
 ?>
